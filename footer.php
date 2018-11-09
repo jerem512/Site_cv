b9d6d665fefe7d50d9c11e40d1a2e6df18ec4fc0
root <jeremymarchal90@gmail.com>
2018-11-09 15:38:52 +0100

    commit

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer id="pied_de_page" class="pied_de_page">
-    <p>Copyright Jérémy, tous droits réservés.</p>
-</footer>
--- index.php
+++ index.php
@@ -2,82 +2,33 @@
 <html>
 	<head>
 		<title>CV en ligne</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<?php include("./assets/assists/header.php") ?>
 	</head>
 	<body class="is-preload">
-
 			<div id="wrapper">
 
 					<section id="main">
 						<header>
-							<span class="avatar"><img src="./assets/images/yolo.jpeg" alt="image" width="122" height="122" /></span>
+							<span class="avatar"><a href="http://localhost/moncv/moncv.php"><img src="./assets/images/yolo.jpeg" alt="image" width="122" height="122" /></a></span>
 							<h1>Jérémy Marchal</h1>
 							<p>Développeur Web débutant</p>
 						</header>
-						<!--
-						<hr />
-						<h2>Extra Stuff!</h2>
-						<form method="post" action="#">
-							<div class="fields">
-								<div class="field">
-									<input type="text" name="name" id="name" placeholder="Name" />
-								</div>
-								<div class="field">
-									<input type="email" name="email" id="email" placeholder="Email" />
-								</div>
-								<div class="field">
-									<select name="department" id="department">
-										<option value="">Department</option>
-										<option value="sales">Sales</option>
-										<option value="tech">Tech Support</option>
-										<option value="null">/dev/null</option>
-									</select>
-								</div>
-								<div class="field">
-									<textarea name="message" id="message" placeholder="Message" rows="4"></textarea>
-								</div>
-								<div class="field">
-									<input type="checkbox" id="human" name="human" /><label for="human">I'm a human</label>
-								</div>
-								<div class="field">
-									<label>But are you a robot?</label>
-									<input type="radio" id="robot_yes" name="robot" /><label for="robot_yes">Yes</label>
-									<input type="radio" id="robot_no" name="robot" /><label for="robot_no">No</label>
-								</div>
-							</div>
-							<ul class="actions special">
-								<li><a href="#" class="button">Get Started</a></li>
-							</ul>
-						</form>
-						<hr />
-						-->
 						<footer>
 							<ul class="icons">
 								<li><a href="https://github.com/jerem512" class="fa-github">GitHub</a></li>
 								<li><a href="mailto:jeremymarchal90@gmail.com" class="fa-envelope-o">mail</a></li>
 								<li><a href="http://localhost/moncv/moncv.php" class="fa-drivers-license-o">CV</a></li>
 							</ul>
+							<ul class="icons">
+								<li><a href="http://monsite/pres_site/" class="fa-file-code-o">Présentation des sites</a></li>
+								<li><a href="#" class="fa-file-code-o">Présentation des sites</a></li>
+								<li><a href="#" class="fa-file-code-o">Présentation des sites</a></li>
+							</ul>
 						</footer>
 					</section>
-					<a href="http://localhost/Site_V2/index.php">oui</a>
-
-					<footer id="footer">
-						<ul class="copyright">
-							<li>&copy; Jérémy</li><li>Design: <a href="http://html5up.net">HTML5 UP</a>. Modifié par Jérémy.</li>
-						</ul>
-					</footer>
-
+					<?php include("./assets/assists/footer.php") ?>
 			</div>
-
-			<script>
-				if ('addEventListener' in window) {
-					window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
-					document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
-				}
-			</script>
+			<script src="./assets/js/script.js"></script>
 
 	</body>
 </html>
